c820b01a92c365e4c327b6fd44f53d8df775746d
Sergey Klevakin <justerest@yandex.ru>
2017-12-10 20:28:31 +0500

    norm

--- src/components/layouts/YoutubeVideo/YoutubeVideo.vue
+++ src/components/layouts/YoutubeVideo/YoutubeVideo.vue
@@ -3,15 +3,3 @@
   <iframe ref="frame" src="https://www.youtube.com/embed/g9cM7a7Mmm4" width="100%" height="400px" frameborder="0"></iframe>
 </div>
 </template>
-
-<script>
-export default {
-  mounted() {
-    const frame = this.$refs['frame'];
-    const height = 0.4 * innerHeight;
-    if (height < parseInt(frame.height)) {
-      frame.height = height + 'px';
-    }
-  },
-};
-</script>
