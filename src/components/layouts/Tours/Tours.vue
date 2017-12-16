1eaa7419db23749631aafab40bf68e4b6bbec4e1
Sergey Klevakin <justerest@yandex.ru>
2017-12-16 14:24:20 +0500

    Release 1.1.1

--- src/components/layouts/Tours/Tours.vue
+++ src/components/layouts/Tours/Tours.vue
@@ -34,9 +34,8 @@ const POINT = /\.|,/;
 
 const CONCERTS = [
   { date: '15.12.17', city: 'РЯЗАНЬ', link: 'https://vk.com/animalzrzn', place: 'Raz Dva Bar' },
-  { date: '16.12.17', city: 'ТВЕРЬ', link: 'https://vk.com/animaljazz_tver', place: 'Милле' },
-  { date: '23.12.17', city: 'ПЕНЗА', link: 'https://vk.com/animal_djazz', place: 'ККЗ Пенза' },
   { date: '27.01.18', city: 'СУРГУТ', link: 'https://vk.com/atmosferasurgut', place: 'Атмосфера' },
+  { date: '03.02.18', city: 'ТВЕРЬ', link: 'https://vk.com/animaljazz_tver', place: 'Милле' },
   { date: '08.02.18', city: 'ИЖЕВСК', link: 'https://vk.com/animal_izh', place: 'Пинта Бар' },
   { date: '15.02.18', city: 'ЧАРТОВА ДЮЖИНА', link: 'https://vk.com/wall-28866484_2111187', place: '' },
   { date: '22.02.18', city: 'ВОРОНЕЖ', link: 'https://vk.com/animalzvrn18', place: 'Station Mir' },
