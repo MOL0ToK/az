85781f71be0cd88fa8b86878e3aabda68626d7d2
Sergey Klevakin <justerest@yandex.ru>
2018-01-05 15:43:08 +0500

    style edits

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,6 @@ import Contacts from '%/components/layouts/Contacts';
 import Chart from '%/components/layouts/Chart';
 import ProjectFooter from '%/components/layouts/Footer';
 
-
 export default {
   name: 'app',
   components: {
