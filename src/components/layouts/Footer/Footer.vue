649d943bbc32bbf14f225a884321ec52856afbbc
Sergey Klevakin <justerest@yandex.ru>
2018-03-25 20:17:15 +0500

    feat(footer): del soundReliz

--- src/components/layouts/Footer/Footer.vue
+++ src/components/layouts/Footer/Footer.vue
@@ -1,25 +1,22 @@
 <template>
-<footer class="footer">
-  <div class="row no-gutters">
-    <div class="container">
-      <div class="row no-gutters align-center justify-center">
-        <a target="_blank" href="http://amtelectronics.com/">
-          <img class="footer__img" src="~%/assets/partners/amt.png" alt="">
-        </a>
-        <a target="_blank" href="http://www.paiste.com/e/endorser_det.php?page=image&endorserid=4946">
-          <img class="footer__img" src="~%/assets/partners/paiste.png" alt="">
-        </a>
-        <a target="_blank" href="http://www.rolandmusic.ru/">
-          <img class="footer__img" src="~%/assets/partners/roland.png" alt="">
-        </a>
-        <a target="_blank" href="http://www.shure.com/">
-          <img class="footer__img" src="~%/assets/partners/shure.png" alt="">
-        </a>
-        <a target="_blank" href="https://soundreliz.com/preorder/animal-jazz">
-          <img class="footer__img" src="~%/assets/partners/soundreliz.png" alt="">
-        </a>
+  <footer class="footer">
+    <div class="row no-gutters">
+      <div class="container">
+        <div class="row no-gutters align-center justify-center">
+          <a target="_blank" href="http://amtelectronics.com/">
+            <img class="footer__img" src="~%/assets/partners/amt.png" alt="">
+          </a>
+          <a target="_blank" href="http://www.paiste.com/e/endorser_det.php?page=image&endorserid=4946">
+            <img class="footer__img" src="~%/assets/partners/paiste.png" alt="">
+          </a>
+          <a target="_blank" href="http://www.rolandmusic.ru/">
+            <img class="footer__img" src="~%/assets/partners/roland.png" alt="">
+          </a>
+          <a target="_blank" href="http://www.shure.com/">
+            <img class="footer__img" src="~%/assets/partners/shure.png" alt="">
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-</footer>
+  </footer>
 </template>
