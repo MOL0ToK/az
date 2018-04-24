35b34fbc022efb49a08587aecc49dbaa9e02c730
Sergey Klevakin <justerest@yandex.ru>
2018-04-24 20:29:31 +0500

    refactor(charts): styles

--- src/components/layouts/Chart/Chart.vue
+++ src/components/layouts/Chart/Chart.vue
@@ -1,57 +1,41 @@
 <template>
-  <section class="chart row no-gutters">
-    <div class="chart__column col-xl-4 col-sm-6">
-      <article class="albom">
-        <a target="_blank"
-           href="https://itunes.apple.com/ru/album/счастье/1359783502">
-          <img class="albom__img"
-               src="~%/assets/happy.jpg"
-               alt="">
-        </a>
-        <section class="albom__actions">
-          <a target="_blank"
-             href="https://itunes.apple.com/ru/album/счастье/1359783502">
-            <img src="~%/assets/itunes.png"
-                 alt="">
+  <section class="chart">
+    <div class="row no-gutters">
+      <div class="chart__column col-xl-4 col-sm-6">
+        <article class="item">
+          <a target="_blank" href="https://itunes.apple.com/ru/album/счастье/1359783502">
+            <img class="chart__img" src="~%/assets/happy.jpg" alt="">
           </a>
-          <a target="_blank"
-             href="https://itunes.apple.com/ru/album/счастье/1359783502">
-            <img src="~%/assets/itunes-buy.png"
-                 alt="">
+          <section class="itunes-buttons">
+            <a class="itunes-buttons__link" target="_blank" href="https://itunes.apple.com/ru/album/счастье/1359783502">
+              <img src="~%/assets/itunes.png" alt="">
+            </a>
+            <a class="itunes-buttons__link" target="_blank" href="https://itunes.apple.com/ru/album/счастье/1359783502">
+              <img src="~%/assets/itunes-buy.png" alt="">
+            </a>
+          </section>
+        </article>
+      </div>
+      <div class="chart__column col-xl-4 col-sm-6">
+        <article class="item">
+          <a target="_blank" href="https://vk.com/market-2571">
+            <img class="chart__img chart__img_mb" src="~%/assets/shop.jpg" alt="">
           </a>
-        </section>
-      </article>
-    </div>
-    <div class="chart__column col-xl-4 col-sm-6">
-      <article class="albom">
-        <a target="_blank"
-           href="https://vk.com/market-2571">
-          <img class="chartova-10__img"
-               src="~%/assets/shop.jpg"
-               alt="">
-        </a>
-        <a class="albom__button mdc-button mdc-button--raised"
-           target="_blank"
-           href="https://vk.com/market-2571">
-          МАГАЗИН
-        </a>
-      </article>
-    </div>
-    <div class="chart__column col-xl-4">
-      <article class="chartova-10">
-        <a target="_blank"
-           href="http://www.nashe.ru/chartova/">
-          <img class="chartova-10__img"
-               src="~%/assets/sound-silence.jpg"
-               alt="">
-        </a>
-        <a class="chartova-10__button mdc-button mdc-button--raised"
-           data-mdc-auto-init="MDCRipple"
-           target="_blank"
-           href="http://www.nashe.ru/chartova/">
-          Голосуй за песню "Звук и тишина"
-        </a>
-      </article>
+          <a class="chart__button mdc-button mdc-button--raised" data-mdc-auto-init="MDCRipple" target="_blank" href="https://vk.com/market-2571">
+            МАГАЗИН
+          </a>
+        </article>
+      </div>
+      <div class="chart__column col-xl-4">
+        <article class="item item_last">
+          <a target="_blank" href="http://www.nashe.ru/chartova/">
+            <img class="chart__img chart__img_mb" src="~%/assets/sound-silence.jpg" alt="">
+          </a>
+          <a class="chart__button mdc-button mdc-button--raised" data-mdc-auto-init="MDCRipple" target="_blank" href="http://www.nashe.ru/chartova/">
+            Голосуй за песню "Звук и тишина"
+          </a>
+        </article>
+      </div>
     </div>
   </section>
 </template>
