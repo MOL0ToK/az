2f1573a22b7f6c471cac542f8485119536dfe5b1
Sergey Klevakin <justerest@yandex.ru>
2018-10-20 13:49:21 +0500

    feat: new header

--- src/components/layouts/Header/Header.vue
+++ src/components/layouts/Header/Header.vue
@@ -1,23 +1,9 @@
 <template>
-<header class="header-section row no-gutters">
-  <div class="header-section__container container">
-    <div class="header-section__row">
-      <div class="hero">
-        <img class="hero__img" src="~%/assets/1.png" alt="">
-      </div>
-      <div class="hero">
-        <img class="hero__img" src="~%/assets/2.png" alt="">
-      </div>
-      <div class="hero">
-        <img class="hero__img" src="~%/assets/3.png" alt="">
-      </div>
-      <div class="hero">
-        <img class="hero__img" src="~%/assets/4.png" alt="">
-      </div>
-      <div class="hero">
-        <img class="hero__img" src="~%/assets/5.png" alt="">
+  <header class="header-section row no-gutters">
+    <div class="header-section__container container">
+      <div class="header-section__row">
+        <img class="header-section__men" src="../../../assets/men.jpg" alt="">
       </div>
     </div>
-  </div>
-</header>
+  </header>
 </template>
