c923cd7376af4bf7c68ba2e90fde1d0f26bba29f
Sergey Klevakin <justerest@yandex.ru>
2019-01-02 20:50:00 +0500

    feat: change video to "Podstava"

--- src/components/layouts/YoutubeVideo/YoutubeVideo.vue
+++ src/components/layouts/YoutubeVideo/YoutubeVideo.vue
@@ -1,5 +1,11 @@
 <template>
-<section class="youtube-video">
-  <iframe ref="frame" src="https://www.youtube.com/embed/g9cM7a7Mmm4" width="100%" height="400px" frameborder="0"></iframe>
-</section>
+  <section class="youtube-video">
+    <iframe
+      ref="frame"
+      src="https://www.youtube.com/embed/iTWjCyCnVOM"
+      width="100%"
+      height="400px"
+      frameborder="0"
+    ></iframe>
+  </section>
 </template>
