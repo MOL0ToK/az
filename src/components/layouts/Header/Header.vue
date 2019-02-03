d25d11126c47ce58d9415eeb100971724ccaf084
Igor Bulygin <boehpyk@gmail.com>
2019-02-03 13:22:14 +0300

    Changed site design

--- src/components/layouts/Header/Header.vue
+++ src/components/layouts/Header/Header.vue
@@ -1,23 +1,42 @@
 <template>
-<header class="header-section row no-gutters">
+<header class="header-section row no-gutters  linear-bgr">
   <div class="header-section__container container">
-    <div class="header-section__row">
-      <div class="hero">
-        <img class="hero__img" src="~%/assets/1.png" alt="">
-      </div>
-      <div class="hero">
-        <img class="hero__img" src="~%/assets/2.png" alt="">
-      </div>
-      <div class="hero">
-        <img class="hero__img" src="~%/assets/3.png" alt="">
-      </div>
-      <div class="hero">
-        <img class="hero__img" src="~%/assets/4.png" alt="">
-      </div>
-      <div class="hero">
-        <img class="hero__img" src="~%/assets/5.png" alt="">
-      </div>
+  </div>
+  <div class="header-section__buttons">
+    <div class="header-section__row container">
+        <div class="concert-link cosmo linear-bgr">
+            <div class="date">
+                8 марта
+            </div>
+            <div class="place">
+                Космонавт
+            </div>
+            <a class="link" href="javascript:void(0);return false;" onclick="pnwidget.show({init:{referral_auth: 'animaldzhazspb'}, event:{alias: 'animal-dzhaz'}, tickets_show: true, exclude_dates: true, customStyle: true, hideHeader: false, closeButton: true})">Купить билет</a>
+        </div>
+        <div class="vote">
+            <a href="https://vk.com/wall-2571_52792" class="linear-bgr" target="_blank">Голосовать</a>
+        </div>
+        <div class="concert-link gorby linear-bgr">
+            <div class="date">
+                14 апреля
+            </div>
+            <div class="place">
+                ДК им. Горбунова
+            </div>
+            <a class="link" href="https://www.concert.ru/Buy?ActionID=85174&ActionDateID=265037&Mode=WithAsyncSave" target="_blank">Купить билет</a>
+        </div>
     </div>
   </div>
+  <div class="header-section__spacer height-10 bg-white"></div>
+  <div class="header-section__vk container">
+      <div class="vk-music hidden-xs-down">
+          <img src="~%/assets/vk-music.png" alt="VK music" />
+      </div>
+      <div class="vk-concerts">Большие концерты по заявкам</div>
+      <div class="nashe-radio hidden-xs-down">
+          <img src="~%/assets/nashe-radio.png" alt="Наше радио" />
+      </div>
+  </div>
+  <div class="header-section__spacer height-20 bg-white"></div>
 </header>
 </template>
